76c2fdeb9f518f667ec45dd4f02f259f32aba96c
wangchaeboll <wang.chaeboll@gmail.com>
2025-07-22 11:37:23 +0800

    clean i dont think the SearchBar.tsx , api.ts, usefetch.ts having problem, its just that the autoFetch = false really useless

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,32 +1,3 @@
-// import { View, Text, Image, TextInput } from 'react-native'
-// import React from 'react'
-// import { icons } from '@/constants/icons'
-
-// interface Props {
-//     placeholder: string,
-//     onPress?: () => void,
-//     value?: string,
-//     onChangeText?: (text: string) => void
-// }
-
-// const SearchBar = ({placeholder, onPress, value, onChangeText }:Props) => {
-//   return (
-//     <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
-//         <Image source={icons.search} className='size-5' resizeMode='contain' tintColor="#ab8bff"/>
-//         <TextInput 
-//             onPress={onPress}
-//             placeholder={placeholder}
-//             value={value}
-//             onChangeText={onChangeText}
-//             placeholderTextColor={"#a88b5d"}
-//             className='flex-1 ml-2 text-white'
-//         />
-//     </View>
-//   )
-// }
-
-// export default SearchBar
-
 import { View, TextInput, Image } from "react-native";
 
 import { icons } from "@/constants/icons";
