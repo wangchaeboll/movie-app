e77438be7d59bb40bb894c7c7f3f63d30efe1942
wangchaeboll <wang.chaeboll@gmail.com>
2025-07-22 11:40:18 +0800

    we going through

--- services/api.ts
+++ services/api.ts
@@ -1,17 +1,3 @@
-// const url = 'https://api.themoviedb.org/3/keyword/keyword_id/movies?include_adult=false&language=en-US&page=1';
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     accept: 'application/json',
-//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3OWM0MTU3OWYzYTMyNzU3Y2IwMzJmMDIwNjJmYWM4YyIsIm5iZiI6MTc1MzA2NDM4MS4wMSwic3ViIjoiNjg3ZGEzYmQzZjY3Y2ZkN2FhNTVjZTEyIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.Bu4QqgfVkjDgT_TIt9bKJmZJXyX8rGqJxjvmaIQmVm8'
-//   }
-// };
-
-// fetch(url, options)
-//   .then(res => res.json())
-//   .then(json => console.log(json))
-//   .catch(err => console.error(err));
-
 export const TMDB_CONFIG = {
     BASE_URL:  'https://api.themoviedb.org/3',
     API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
